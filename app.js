e9c08a051fc13e47bf6ab3bbcb4d2fe6164b8aea
Thouseef ALI <thouseefali@outlook.com>
2022-02-15 16:36:49 +0000

    listening on port 3030

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ app.use(function(request, response, next) {
     console.log("In comes a " + request.method + " to " + request.url);
     next();
 });
+
+// Request Handlers
+app.get('/', (req, res) => {
+    res.send("OK");
+});
+
+//Listening on port 3030
+app.listen(3030);
