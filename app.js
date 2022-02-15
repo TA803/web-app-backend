fd91a4c629f86e328375fd810615be9651f609a8
Thouseef ALI <thouseefali@outlook.com>
2022-02-15 16:29:16 +0000

    added cors and logging

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+// importing modules
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
+
+// allows cross origin requests
+app.use(cors());
+
+// logging
+app.use(function(request, response, next) {
+    console.log("In comes a " + request.method + " to " + request.url);
+    next();
+});
