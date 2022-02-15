e1139256f0e7e85fb9e2828149ee74984f20317d
Thouseef ALI <thouseefali@outlook.com>
2022-02-15 17:29:34 +0000

    static files and mongoDB

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const app = express();
 const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 
+//connects to mongoDB
+let db;
+MongoClient.connect("mongodb+srv://afterschlClub:AfterschlClub@cluster0.yboho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", (err, client) => {
+    db = client.db("AfterSchoolClub")
+})
+
 // allows cross origin requests
 app.use(cors());
+
+//loads images from folder
+app.use(express.static("images"))
 
 // logging
 app.use(function(request, response, next) {
